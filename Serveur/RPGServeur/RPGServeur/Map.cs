21c536276a642c8a7df59dca847ab54cc5e72e5b
christopheK <christophe.kmbr@eduge.ch>
2017-02-09 13:47:14 +0100

    classes Serveur

---
+++ Serveur/RPGServeur/RPGServeur/Map.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGServeur
+{
+    class Map
+    {
+        int[,] table;
+    }
+}
---
+++ Serveur/RPGServeur/RPGServeur/Player.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGServeur
+{
+    class Player
+    {
+        private int _id;
+        private string _username;
+        private IPAddress _ip;
+        private Point _position;
+
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+
+            set
+            {
+                _id = value;
+            }
+        }
+
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+
+            set
+            {
+                _username = value;
+            }
+        }
+
+        public IPAddress Ip
+        {
+            get
+            {
+                return _ip;
+            }
+
+            set
+            {
+                _ip = value;
+            }
+        }
+
+        public Point Position
+        {
+            get
+            {
+                return _position;
+            }
+
+            set
+            {
+                _position = value;
+            }
+        }
+
+        public Player(int id, string username, IPAddress ip, Point position)
+        {
+            this._id = id;
+            this._username = username;
+            this._ip = ip;
+            this._position = position;
+        }
+    }
+}
